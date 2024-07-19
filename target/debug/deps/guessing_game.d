c3a474f8f9e05f90c9f4fcf69df7ba026f0b21f6
quantumvflux <marcosabaravalle@gmail.com>
2024-07-19 11:28:23 -0300

    add target folder to .gitignore

--- target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-E:\dev\rust_book\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-E:\dev\rust_book\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
--- target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-E:\dev\rust_book\guessing_game\target\debug\guessing_game.exe: E:\dev\rust_book\guessing_game\src\main.rs
